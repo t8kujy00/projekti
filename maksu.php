2c6230eafa8e521b48a732982bb7bfce14778690
Jyri Kuusela <t8kujy00@students.oamk.fi>
2019-10-14 14:16:13 +0300

    html sivut luotu

---
+++ maksu.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Maksaminen</title>
+  </head>
+  <body>
+    <div class="navbar">
+  <a href="paasivu.php"><b>Etusivu</b></a>
+<a href="login.php"><b>Kirjaudu ulos</b></a>
+</div>
+<div align="center">
+  <div id="header">
+<h1><b>Maksaminen</b></h1><br><br>
+</div>
+    <form action="">
+  <b>Saajan tilinumero:</b><br>
+  <input type="text"><br>
+  <b>Määrä:</b><br>
+  <input type="text"><br>
+  <b>Viitenumero:</b><br>
+  <input type="text"><br>
+  <b>Viesti:</b><br>
+  <input type="text"><br><br>
+</form>
+<button class="btn default"><a href="#">Maksa</a></button>
+</div>
+
+  </body>
+</html>
---
+++ tili.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <div class="navbar">
+  <a href="paasivu.php"><b>Etusivu</b></a>
+  <a href="login.php"><b>Kirjaudu ulos</b></a>
+</div>
+</div>
+    <div align="center">
+    <div id="header">
+      <h1><b>Tilisi tämänhetkinen saldo: 322,09</b></h1><br><br>
+        <h2><b>Tapahtumat:</b></h2><br><br>
+    </div>
+    <div class="main">
+    <div align"center">
+      <table style="width:50%">
+      <tr>
+        <th>Päivämäärä</th>
+        <th>Saaja/Maksaja</th>
+        <th>Määrä</th>
+      </tr>
+      <tr>
+        <td>19.9.2019</td>
+        <td>PRISMA RAKSILA OULU</td>
+        <td>-25,43</td>
+      </tr>
+      <tr>
+        <td>13.9.2019</td>
+        <td>PAYPAL HBONORDIC</td>
+        <td>-9,87</td>
+      </tr>
+    </table>
+</div>
+
+
+
+  </body>
+</html>
